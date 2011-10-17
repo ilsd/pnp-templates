7f68c7ef1ccc37eb531d261a77648730a02c3394
Stefan Senftleben <stefan.senftleben@itsc.de>
2011-10-17 18:35:38 +0200

    Update auf die letzte Version

--- check-netapp-PerfSys.php
+++ check-netapp-PerfSys.php
@@ -1,24 +1,24 @@
-<?php
-#
-# Copyright (c) 2006-2010 Joerg Linge (http://www.pnp4nagios.org)
-# Plugin: check_icmp [Multigraph]
-#
-# RTA
-#
-$ds_name[1] = "$servicedesc";
-$opt[1]  = "--units-exponent=0 --lower=0 --vertical-label \"$UNIT[1]\" --title \"'$ds_name[1]'\" --watermark=http://netapp-monitoring.info" ;
-$def[1]  = rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE") ;
-$def[1] .= rrd::area( "var1", "#AAB559",  $ds_name[1] );
-$def[1] .= rrd::line1( "var1", "#0459b3" );
-$def[1] .= rrd::gprint("var1", array("LAST", "MAX", "AVERAGE"), "%6.2lf $UNIT[1]") ;
-  
-if($WARN[1] != ""){
-    if($UNIT[1] == "%%"){ $UNIT[1] = "%"; };
-    $def[1] .= rrd::hrule($WARN[1], "#ffff00", "Warning  ".$WARN[1].$UNIT[1]."\\n");
-}
-if($CRIT[1] != ""){
-   if($UNIT[1] == "%%"){ $UNIT[1] = "%"; };
-   $def[1] .= rrd::hrule($CRIT[1], "#ff0000", "Critical  ".$CRIT[1].$UNIT[1]."\\n");
-}
-
-?>
+    <?php
+    #
+    # Copyright (c) 2006-2010 Joerg Linge (http://www.pnp4nagios.org)
+    # Plugin: check_icmp [Multigraph]
+    #
+    # RTA
+    #
+    $ds_name[1] = "$servicedesc";
+    $opt[1]  = "-I --units-exponent=0 --lower=0 --vertical-label \"$UNIT[1]\" --title \"'$ds_name[1]'\" --watermark=http://netapp-monitoring.info" ;
+    $def[1]  = rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE") ;
+    $def[1] .= rrd::area( "var1", "#AAB559",  $ds_name[1] );
+    $def[1] .= rrd::line1( "var1", "#0459b3" );
+    $def[1] .= rrd::gprint("var1", array("LAST", "MAX", "AVERAGE"), "%6.2lf $UNIT[1]") ;
+     
+    if($WARN[1] != ""){
+        if($UNIT[1] == "%%"){ $UNIT[1] = "%"; };
+        $def[1] .= rrd::hrule($WARN[1], "#ffff00", "Warning  ".$WARN[1].$UNIT[1]."\\n");
+    }
+    if($CRIT[1] != ""){
+        if($UNIT[1] == "%%"){ $UNIT[1] = "%"; };
+        $def[1] .= rrd::hrule($CRIT[1], "#ff0000", "Critical  ".$CRIT[1].$UNIT[1]."\\n");
+    }
+     
+    ?>
