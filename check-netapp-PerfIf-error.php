2ca1a34d0ee058e7a2520ce2d6d036ffc08c27af
Ingo Lantschner <nagios-plugins@lantschner.name>
2011-10-18 08:55:08 +0200

    Codecleaning und Licenses (GPL)
        new file:   COPYING
        modified:   check-netapp-Hardware.php
        modified:   check-netapp-PerfCpu.php
        modified:   check-netapp-PerfDisk.php
        modified:   check-netapp-PerfIf-error.php
        modified:   check-netapp-PerfIf-traffic.php
        modified:   check-netapp-PerfSys.php
        modified:   check-netapp-PerfVolume.php
        modified:   check-netapp-Snapshots.php
        modified:   check-netapp-Usage.php
        deleted:    setup-instructions-templates.txt

--- check-netapp-PerfIf-error.php
+++ check-netapp-PerfIf-error.php
@@ -1,10 +1,20 @@
     <?php
     #
-    # Copyright (c) 2006-2010 Joerg Linge (http://www.pnp4nagios.org)
-    # Plugin: check_icmp [Multigraph]
-    #
-    # RTA
-    #
+    # Copyright (c) 2011 Ingo LANTSCHNER (http://netapp-monitoring.info)
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+    # 
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+    # 
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
     $ds_name[1] = "$servicedesc";
     $opt[1]  = "-I --units-exponent=0 -Y --lower=0 --vertical-label \"Errors $UNIT[1]\" --title \"'$ds_name[1]'\" --watermark=http://netapp-monitoring.info";
     $def[1]  = rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE") ;
